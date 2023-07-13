a1697ec79b9c64926fe3ac18cc566354facbc95b
Othmane <offissayegh001@gmail.com>
2023-07-13 16:59:21 +0100

    Finish html of the header part

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-
+import "../../style/Header.css";
+import { Link } from "react-router-dom";
+import lightMode from "./images/lightMode.png";
 
 function Header() {
-	return <h1>Hello From Header</h1>;
+	// The header will have its own color and the background a little bit similar	
+	return (
+		<header>
+			<div>
+				{/* <img></img> */}
+				<h1>Crypto Website</h1>
+			</div>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/news">News</Link>
+				</li>
+				<li>
+					<Link to="/cryptocurrencies">Market</Link>
+				</li>
+				<li className="last-li">
+					<img src={lightMode} width="23px"/>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
