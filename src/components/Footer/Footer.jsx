15493a3ca801349cdc12b1b9f99d33635945078e
Othmane <offissayegh001@gmail.com>
2023-07-15 14:41:32 +0100

    Add linkedin and github icons for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from "react"
+import "../../style/Footer.css"
+import linkedinIcon from "./linkedinIcon.png"
+import githubIcon from "./githubIcon.png"
 
 function Footer(props) {
-	return <h1>Hello from Footer</h1>;
+	return <footer>
+		<div id="footer-last-part">
+			<p className="author">
+				Made by Othmane Essayegh
+				<img src={linkedinIcon} width="22px"/>
+				<img src={githubIcon} width="22px"/>
+			</p>
+		<p>Copyright &copy; 2023 Crypto Website.&nbsp; All rights reserved.</p>
+		</div>
+	</footer>;
 }
 
 export default Footer;
